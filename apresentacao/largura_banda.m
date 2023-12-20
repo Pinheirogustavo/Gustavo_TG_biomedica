c79724aae23264411c0071c1a8ebd44dd10fd5f4
pinheirogustavo <gustavopinheirozz@gmail.com>
2023-12-19 22:45:22 -0300

    dados sr ad826

--- apresentacao/largura_banda.m
+++ apresentacao/largura_banda.m
@@ -1,15 +1,17 @@
 x = logspace(-2,1);
-y = (0.5/(10**-6)) ./ (2*pi*x); #slew rate 0,5 u/V OP07C
+y = (0.5/(10**-6)) ./ (2*pi*x); #slew rate 0,5 u/V tl072 real
 y2 = (350/(10**-6)) ./ (2*pi*x); #slew rate 350 u/V AD826
 y1 = (16/(10**-6)) ./ (2*pi*x); #slew rate 13 u/V TL072C
 y3 = (450/(10**-6)) ./ (2*pi*x); #slew rate 450 u/V AD828
+y4 = (32/(10**-6)) ./ (2*pi*x); #slew rate 32 u/V Ad826real
+
 Z2M = linspace(2e+06,2e+06,50);
 Z50k = linspace(50e+03,50e+03,50);
 
-loglog(x,y, "linewidth", 2,x,y1, "linewidth", 2,x,y2, "linewidth", 2,x,y3 , "linewidth", 2, x,Z2M,'--',"linewidth", 2, x,Z50k,'--',"linewidth", 2);
+loglog(x,y, "linewidth", 2,x,y1, "linewidth", 2,x,y4,  "linewidth", 2,x,y2, "linewidth", 2,x,y3 ,"linewidth", 2, x,Z2M,'--',"linewidth", 2, x,Z50k,'--',"linewidth", 2);
 grid on
 
-legend('TL072 - Real - 0,5 u/V','TL072 - nominal - 13 u/V ','AD826 - 350 u/V','AD828 - 450 u/V', '2 MHz','50 kHz','Location','northeast')
+legend('TL072 - Real - 0,5 u/V','TL072 - nominal - 13 u/V ', 'AD826 - Real - 32 u/V','AD826 - Nominal - 350 u/V','AD828 - Nominal - 450 u/V','2 MHz','50 kHz','Location','northeast')
 xlabel ('Tensão de pico (V)')
 ylabel ('Largura de banda de grande sinal (Hz)')
 #title ('dddd')
